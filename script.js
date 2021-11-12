5d5be9c4214cf8d0a0e9dbf111e92d71eecfc739
jsurmaczBU <79499198+jsurmaczBU@users.noreply.github.com>
2021-11-12 16:29:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+function logMessage(name, msg) {
+	// your code here
+}
+
+// TESTS (unhide the function calls by removing the comments preceding them)
+
+// logMessage("Amanda", "Goodbye");
+// logMessage(4, "Hello");
+// logMessage("Amanda", 4);
+// logMessage(4, 4);
